38dfc41759ef1d866d244ef8e59ff21900b035fc
Mystery0A0 <119829331+Mystery0A0@users.noreply.github.com>
2022-12-16 14:51:33 -0500

    Add files via upload
    update version no other import class

--- src/finalproject/GraphTraversal.java
+++ src/finalproject/GraphTraversal.java
@@ -2,56 +2,51 @@ package finalproject;
 
 import finalproject.system.Tile;
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
 
 public class GraphTraversal
 {
 
-
 	//TODO level 1: implement BFS traversal starting from s
 	public static ArrayList<Tile> BFS(Tile s)
 	{
-		Queue<Tile> queue = BFS_helper(s);
-		return new ArrayList<>(queue);
-	}
-
-	public static Queue<Tile> BFS_helper(Tile s){
-		Queue<Tile> queue1 = new LinkedList<>();
-		Queue<Tile> queue2 = new LinkedList<>();
-		if(s.isWalkable())
-			queue1.add(s);
+		LinkedList<Tile> queue1 = new LinkedList<>();
+		ArrayList<Tile> queue2 = new ArrayList<>();
+		queue1.add(s);
 		while(!queue1.isEmpty()){
-			Tile current  = queue1.poll();
+			Tile current = queue1.get(0);
+			queue1.remove(0);
 			queue2.add(current);
-			for(Tile t : current.neighbors){
-				if(!queue1.contains(t) && !queue2.contains(t))
-					queue1.add(t);
+			for (Tile t: current.neighbors) {
+				if (t.isWalkable()){
+					if (!queue1.contains(t) && !queue2.contains(t))
+						queue1.add(t);
+				}
 			}
 		}
 		return queue2;
 	}
 
-
 	//TODO level 1: implement DFS traversal starting from s
 	public static ArrayList<Tile> DFS(Tile s) {
-		Queue<Tile> queue = DFS_helper(s);
-		return new ArrayList<>(queue);
-	}
+		LinkedList<Tile> stack = new LinkedList<>();
+		ArrayList<Tile> queue = new ArrayList<>();
 
-	public static Queue<Tile> DFS_helper(Tile s){
-		Stack<Tile> stack = new Stack<>();
-		Queue<Tile> queue = new LinkedList<>();
-		stack.push(s);
-		while(!stack.isEmpty()){
-			Tile current = stack.pop();
-			queue.add(current);
-			for(Tile t : current.neighbors){
-				if(!stack.contains(t) && !queue.contains(t))
-					stack.push(t);
-			}
+		stack.add(s);
+		while (!stack.isEmpty()){
+			Tile current = stack.get(stack.size() - 1);
+				stack.remove(stack.size() - 1);
+				queue.add(current);
+				for (Tile t: current.neighbors) {
+					if (t.isWalkable()){
+						if (!stack.contains(t) && !queue.contains(t))
+							stack.add(t);
+					}
+				}
 		}
 		return queue;
 	}
-
 }  
